931bd20a9f47b6a22fdada36f4ee0e6e2371fcb6
andipyk <Andiokey123>
2020-09-05 21:22:15 +0700

    Initial commit

---
+++ src/main/java/com/example/rabbitmq003/Rabbitmq003Application.java
@@ -0,0 +1,13 @@
+package com.example.rabbitmq003;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rabbitmq003Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Rabbitmq003Application.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/rabbitmq003/rabbitmq/RestApiReceive.java
@@ -0,0 +1,28 @@
+package com.example.rabbitmq003.rabbitmq;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+public class RestApiReceive {
+    public static void main(String[] args) throws IOException, TimeoutException {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        channel.queueDeclare("insertSiswa", false, false, false, null);
+        System.out.println(" [*] Waiting for messages from database");
+
+        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+            String message = new String(delivery.getBody(), "UTF-8");
+            System.out.println(" [x] Received '" + message + "'");
+        };
+        channel.basicConsume("insertSiswa", true, deliverCallback, consumerTag -> { });
+    }
+
+}
